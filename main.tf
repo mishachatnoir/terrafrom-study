66ae72f937ae97bee1005c24b433b6035184ab9b
MishaChatNoir <misha@DESKTOP-NICPFBD.localdomain>
2023-01-21 19:24:34 +0300

    revert

--- main.tf
+++ main.tf
@@ -1,62 +1,15 @@
-variable "server_port" {
-  description="The port the server will use for HTTP requests"
-  type=number
-  default="8080"
-}
-
-output "public_ip" {
-  value = aws_instance.example.public_ip
-  description="The public IP addressof the web server"
-}
-
 provider "aws" {
   region = "us-east-2"
-}
-
-data "aws_vpc" "default" {
-  vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port = var.server_port
-    to_port = var.server_port
+    from_port = 8080
+    to_port = 8080
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_launch_configuration" "example" {
-  image_id      = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-  security_groups = [aws_security_group.instance.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World!" -> index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
-
-  # Required when using a launch configuration with an auto scaling group.
-  # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier = data.aws_subnet_ids.default.ids
-
-  max_size = 10
-  min_size = 2
-
-  tag {
-    key                 = "Name"
-    propagate_at_launch = "terraform-asg-example"
-    value               = true
   }
 }
 
@@ -68,69 +21,10 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
+              nohup busybox httpd -f -p 8080 &
               EOF
 
     tags = {
       Name = "terraform-example"
-  }
-}
-
-resource "aws_lb" "example" {
-  name = "terraform-asg-example"
-  load_balancer_type = "application"
-  subnets = data.aws_subnet_ids.default.ids
-  security_groups = [aws_security_group.alb.id]
-}
-
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.example.arn
-  port = 80
-  protocol = "HTTP"
-
-  # By default, return a simple 404 page
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code = 404
-    }
-  }
-}
-
-resource "aws_security_group" "alb" {
-  name = "terraform-example-alb"
-
-  #Allow inbound HTTP requests
-  ingress {
-    from_port = 80
-    protocol  = "HTTP"
-    to_port   = 80
-    cidr_blocks = [0.0.0.0/0]
-  }
-  #Allow all outbound requests
-  egress{
-    from_port = 0
-    to_port = 0
-    protocol = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_lb_target_group" "asg" {
-  name = "terraform-asg-example"
-  port = var.server_port
-  protocol = "HTTP"
-  vpc_id = data.aws_vpc.default.id
-  health_check {
-    path = "/"
-    protocol = "HTTP"
-    matcher = "200"
-    interval = 15
-    timeout = 3
-    healthy_threshold = 2
-    unhealthy_threshold = 2
   }
 }
