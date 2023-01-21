9aae79ac74c073dde41f3a921bb99f60f8c4447a
MishaChatNoir <misha@DESKTOP-NICPFBD.localdomain>
2023-01-21 20:39:28 +0300

    aws launch config

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ output "public_ip" {
   description = "The public IP address of the web server"
 }
 
+
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
@@ -38,4 +39,16 @@ resource "aws_instance" "example" {
     tags = {
       Name = "terraform-example"
   }
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id      = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World!" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
 }
